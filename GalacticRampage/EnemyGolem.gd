1cea821c1ecd885e26d69f3aa6c1fa90d2fece83
Connor Coleman <concol27@kingshigh.school.nz>
2024-08-14 14:57:26 +1200

    Enemy shooting and level design
    The enemy now shoots. I am yet to code the bullets to travel towards the player and make it kill the player. I have also started my level design.

--- GalacticRampage/EnemyGolem.gd
+++ GalacticRampage/EnemyGolem.gd
@@ -6,6 +6,9 @@ const JUMP_VELOCITY = -400.0
 @onready var sprite = $Sprite
 @onready var player = get_tree().get_first_node_in_group("Player")
 const EXPLOSION = preload("res://explosion.tscn")
+const KNIFE_2 = preload("res://knife2.tscn")
+@onready var world = get_node('/root/world')
+var direction = Vector2.ZERO
 
 func check_animation():
 	if velocity == Vector2.ZERO:
@@ -15,7 +18,7 @@ func check_animation():
 
 	# Declare variables for the speed and the detection range
 var SPEED = 50  # Adjust this to your desired speed
-var DETECTION_RANGE = 200  # Adjust this to your desired detection range
+var DETECTION_RANGE = 150  # Adjust this to your desired detection range
 
 func _physics_process(delta):
 	# Calculate the direction to the player
@@ -31,10 +34,15 @@ func _physics_process(delta):
 		# If outside the detection range, set velocity to zero
 		velocity = Vector2.ZERO
 
+
+
 	# Apply the velocity to the enemy (assuming you're using a KinematicBody2D or similar)
 	move_and_slide()
 	check_animation()
 	
+
+
+
 	if velocity.x > 0: 
 		sprite.flip_h = true 
 	else:
@@ -47,3 +55,11 @@ func enemy_die():
 	var new_explosion = EXPLOSION.instantiate()
 	new_explosion.global_position = global_position
 	add_sibling(new_explosion)
+
+
+
+func _on_knife_timer_timeout():
+	var knife = KNIFE_2.instantiate()
+	knife.global_position = global_position
+	knife.rotate(direction.angle())           
+	world.add_child(knife)
---
+++ GalacticRampage/knife2.gd
@@ -0,0 +1,11 @@
+extends Area2D
+
+@export var SPEED = 500
+
+func _ready():
+	pass
+
+
+
+func _process(delta):
+	translate(Vector2.RIGHT.rotated(rotation) * SPEED * delta)
